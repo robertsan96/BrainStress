00c195ffbf66c9bf7259099a3ce4caa13d831831
robertsan96 <robertsandru96@gmail.com>
2020-10-05 18:40:07 +0300

    Initial Commit

---
+++ BrainStress/BrainStressApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainStressApp.swift
+//  BrainStress
+//
+//  Created by Robert Sandru on 05/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct BrainStressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrainStress/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BrainStress
+//
+//  Created by Robert Sandru on 05/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
